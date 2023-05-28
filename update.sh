d5c791dc2ff8b2ee498f85e970c6b9fec03ea360
bloodchen <bloodchen@gmail.com>
2023-05-28 13:39:33 +0800

    update

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
+cp data/config_pv.js data/config.js
 docker pull bloodchen/nbdb
 docker-compose up -d
--- update_dev.sh
+++ update_dev.sh
@@ -1,2 +1,3 @@
+cp data/config_mx1.js data/config.js
 docker pull bloodchen/nbdb_dev
 docker-compose -f dev-compose.yaml up -d
