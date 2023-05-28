9f0f8d933099868eae1e85042be76de0d7e3b129
bloodchen <bloodchen@gmail.com>
2023-05-28 12:42:47 +0800

    update

--- mx1_update.sh
+++ mx1_update.sh
@@ -1,3 +1,3 @@
 cp data/config_mx1.js data/config.js
 docker pull bloodchen/nbdb
-docker-compose up -d
+docker run --name nb_mx1 -p 9100:9000 -v ./data:/home/node/app/data --restart=always bloodchen/nbdb:latest
